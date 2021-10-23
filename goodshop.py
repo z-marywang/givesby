ba2140d13ef673a213f26ac0d13277e43f351209
Maverick Chung <mc608@duke.edu>
2021-10-23 18:49:11 -0400

    added goodshop urls

---
+++ goodshop.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3.9
+import requests
+import re
+
+url = "https://www.goodshop.com/coupons/all/"
+rx = 'class="link coupons" href="/coupons/(.*?)"'
+num_pages = 254
+
+links = []
+
+for i in range(1, num_pages+1):
+    content = requests.get(url+str(i)).text
+    links += [m.group(1) for m in re.finditer(rx, content)]
+    if i % 10 == 0:
+        print(f"{i+1} of {num_pages} pages scraped")
+
+print(links)
+
+print(f"\n{len(links)} links found")
+
+f = open("goodshop_links.txt", "w")
+[f.write(i+'\n') for i in links]
